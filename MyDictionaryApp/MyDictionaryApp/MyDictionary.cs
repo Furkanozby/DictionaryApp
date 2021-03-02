7fadefae6e1efd7d4a058d0f2b9cf7bd9b36f77e
Furkan Özbay <furkanozb37@hotmail.com>
2021-03-03 01:52:13 +0300

    Tamamlandı.

---
+++ MyDictionaryApp/MyDictionaryApp/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryApp
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempArrayKey = keys;
+            Value[] tempArrayValue = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+                values[i] = tempArrayValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " -> " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryApp/MyDictionaryApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, short> cities = new MyDictionary<string, short>();
+            cities.Add("Kastamonu", 37);
+            cities.Add("İstanbul", 34);
+            cities.Add("Ankara", 06);
+            cities.Add("Konya", 42);
+            cities.Add("Şanlıurfa", 63);
+            cities.Add("Hatay", 31);
+
+
+            cities.List();
+            Console.Read();
+        }
+    }
+}
